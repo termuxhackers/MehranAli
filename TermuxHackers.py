03a17d4d50fdaeb043ca1319202e8206b7f22ad7
termuxhackers <73843328+termuxhackers@users.noreply.github.com>
2020-11-02 22:27:42 +0500

    Update TermuxHackers.py

--- TermuxHackers.py
+++ TermuxHackers.py
@@ -214,7 +214,6 @@ while (loop == 'true'):
     	password = raw_input("\033[1;96m[☆] \x1b[1;93mPassword Of Tool \x1b[1;96m>>>> ")
         if (password == CorrectPassword):
             print "Logged in successfully as " + username
-
       loop = 'false'
         else:
             print "Wrong Password"
